6b161e53ad474de01d3e9ef6843cd89c275fdea6
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-07-24 15:56:55 +0200

    Created XCode project

---
+++ NightWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by Rafał Chałupnik on 24/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Rafał Chałupnik on 24/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
