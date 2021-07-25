73ff820d26e5e2fba7c08c99744fa59f2d84c806
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-07-25 17:47:03 +0200

    Further visual touches

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -12,50 +12,66 @@ struct ContentView: View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
-                    HStack {
-                        Text(Image(systemName: "moon.stars"))
-                            .foregroundColor(.yellow)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Nightly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(.yellow)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    // MARK: Nightly Tasks
+                    Group {
+                        HStack {
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Nightly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("- Check all windows")
+                        Text("- Check all doors")
+                        Text("- Check that the safe is locked")
+                        Text("- Check the mailbox")
+                        Text("- Inspect security cameras")
+                        Text("- Clear ice from sidewalks")
+                        Text("- Document \"strange and unusual\" occurences")
                     }
-                    Text("Check all windows")
-                    Text("Check all doors")
-                    Text("Check that the safe is locked")
-                    Text("Check the mailbox")
-                    Text("Inspect security cameras")
-                    Text("Clear ice from sidewalks")
-                    Text("Document \"strange and unusual\" occurences")
-                    HStack {
-                        Text(Image(systemName: "sunset"))
-                            .foregroundColor(.yellow)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Weekly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(.yellow)
-                            .padding(.top)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    // MARK: Weekly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "sunset"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("- Check inside all vacant rooms")
+                        Text("- Walk the perimiter of property")
                     }
-                    HStack {
-                        Text(Image(systemName: "calendar"))
-                            .foregroundColor(.yellow)
-                            .font(.title3)
-                            .fontWeight(.heavy)
-                        Text("Monthly Tasks")
-                            .underline()
-                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                            .fontWeight(.heavy)
-                            .foregroundColor(.yellow)
-                            .padding(.top)
-                            .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                    // MARK: Monthly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "calendar"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("- Test security alarm")
+                        Text("- Test motion detectors")
+                        Text("- Test smoke alarms")
                     }
                 }
                 .foregroundColor(.gray)
