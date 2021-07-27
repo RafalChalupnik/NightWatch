e9acc71e1ebb168bccc959aed8a337afd114e869
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-07-27 19:16:26 +0200

    Added Task model

---
+++ NightWatch/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  NightWatch
+//
+//  Created by Rafał Chałupnik on 27/07/2021.
+//
+
+import Foundation
+
+struct Task {
+    let name: String
+    var isComplete: Bool
+    var lastComplete: Date?
+}
