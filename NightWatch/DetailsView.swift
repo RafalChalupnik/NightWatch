2196f1f4ee858345c0f54645f2b2b8c590350875
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-08-02 16:56:01 +0200

    Adaptive UI

--- NightWatch/DetailsView.swift
+++ NightWatch/DetailsView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct DetailsView: View {
     @Binding var task: Task
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     var body: some View {
         VStack {
+            HStack {
+                Image(systemName: "map")
+                Text("Placeholder for motel floor plan")
+            }
             Text(task.name)
-            Text("Placeholder for task description")
+            
+            if (verticalSizeClass == .regular) {
+                Divider()
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis vestibulum nisi, non sollicitudin nunc. Donec nec ex placerat, ultrices lorem a, posuere massa. Nunc varius neque eget tellus fringilla porttitor in malesuada sem. Aliquam eu magna non nisi laoreet rutrum. Phasellus in facilisis enim, quis ornare magna. In hac habitasse platea dictumst.")
+            }
+            
+            Divider()
             Button("Mark Complete") {
                 task.isComplete = true
             }
@@ -23,6 +34,10 @@ struct DetailsView: View {
 
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailsView(task: .constant(Task(name: "Test task", isComplete: false, lastComplete: nil)))
+        Group {
+            DetailsView(task: .constant(Task(name: "Test task", isComplete: false, lastComplete: nil)))
+            DetailsView(task: .constant(Task(name: "Test task", isComplete: false, lastComplete: nil)))
+                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/667.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/375.0/*@END_MENU_TOKEN@*/))
+        }
     }
 }
